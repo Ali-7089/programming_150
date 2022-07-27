79d5a01f517b5a61f478a6c55e0cb716505d2e9b
Ali-7089 <68490238+Ali-7089@users.noreply.github.com>
2022-07-27 12:45:04 +0530

    Update Ques_16.java

--- src/Ques_16.java
+++ src/Ques_16.java
@@ -6,8 +6,7 @@ public class Ques_16 {
         double principle = sc.nextDouble(), rate = sc.nextDouble(), time = sc.nextDouble();
 
         /* Calculate compound interest */
-        double CI = principle *
-                (Math.pow((1 + rate / 100), time));
+        double CI = (principle * (Math.pow((1 + rate / 100), time)))-principle;
         System.out.println(CI);
     }
 }
