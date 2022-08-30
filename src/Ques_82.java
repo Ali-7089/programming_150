4bd3a70b1053baf87a75f545774167d69b736297
Ali-7089 <68490238+Ali-7089@users.noreply.github.com>
2022-08-30 14:28:58 +0530

    Update Ques_82.java

--- src/Ques_82.java
+++ src/Ques_82.java
@@ -6,10 +6,10 @@ public class Ques_82 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
         int arr[] = new int[n];
         for (int i=0;i< arr.length;i++)arr[i] = sc.nextInt();
-
+        int k = sc.nextInt();
+        if (k>= arr.length)k%= arr.length;
         //   FIRST APPROACH
        /* int num[] = new int[k];
         for(int i=0;i<num.length;i++){
